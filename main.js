337990c1c6d2ef7b5f390f0f5fa0b4b80e085030
asoolito <roblox890king890@gmail.com>
2024-05-08 09:27:01 +0500

    JAVA 2

---
+++ main.js
@@ -0,0 +1,43 @@
+// var ism = "polat";
+// ism = "ibrohim";
+// var ism = "Abduraxmon";
+
+
+// let ism = "polat"
+// ism = "ibrohim"
+//  let ism = "Abduraxmon"
+// console.log(ism)
+
+// const   ism ="polat"
+// //  ism = "ibrohim"              
+// console.log(ism) 
+// let userName = prompt("Please enter your name")
+// let userSurName = prompt("Please enter your Surnemember")
+
+// console.log(userName   + userS   urName)
+// let usernum1 = +prompt ("Please enter your num1");
+// let usernum2 = +prompt ("Please enter your num2");
+// console.log(usernum1-usernum2);
+
+// let userName = prompt("ism kiriting")
+// let userSurName = prompt("familiyangizni kirinting")
+// let jajax = alert("malumotlaringiz uchun raxmat ")
+// let javoblar = alert(`ism:${userName} ` )
+
+let userName = prompt("string typida ma'lumot kiriting")
+console.log(userName);
+let userNum = +prompt("numberv typida ma'lumot kiriting")
+console.log(userNum);
+let userBoolean = prompt("boolean typeda ma'lumot kiriting")
+
+let register = confirm  ("hamma ma'lumotni to'g'ri kiritganingizga ishonchingiz komilmi")
+
+
+// alert( userBoolean, userName , UserNum)
+alert(`
+siz kiritgan (${userName})string typida edi
+siz kiritgan (${userNum})number typida edi
+siz kiritgan (${userBoolean})Boolean typida edi`)
+
+
+
